9f2fefcc9cd322e2fef970434c21a45708b2f7a2
Vaibhav Kumar Vishwakarma <vishwakarmavaibhav742@gmail.com>
2026-03-03 06:40:46 +0530

    vercel

--- chatkara_connect/wsgi.py
+++ chatkara_connect/wsgi.py
@@ -2,3 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatkara_connect.settings')
 application = get_wsgi_application()
+
+# Vercel serverless function expects a variable named 'app'
+app = application
